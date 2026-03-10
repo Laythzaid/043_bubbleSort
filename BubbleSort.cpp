05b1fc1ec7175c16c537120defcf784a8a545217
Layth Zaid <Laithhilal23@gmail.com>
2026-03-10 15:55:33 +0700

    add display() method

---
+++ BubbleSort.cpp
@@ -0,0 +1,72 @@
+//Bubble Sort Algorithm is C++
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int arr[20]; //Global variable: array with size 20
+int n;       //Global variable: to store number of elements in the array
+void input()
+{
+    //input code
+    int i;
+    while(true) 
+    {
+    cout << "Enter the number of elements in the array: "; //Output to screen
+    cin >> n; // Input from user
+    if (n <= 20)
+        break; // Exit loop if less than or equal to 20
+        else 
+        {
+            cout << "\nArray can have a maximum of 20 elements.\n"; //Output message
+        }
+
+    }
+    
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Enter Array Elements" << endl;
+    cout << "=====================" << endl;
+    for (int i = 0; i < n; i++) //loop form 0 to n-1
+    {
+        cout << "Data-" << (i + 1) << ": "; //Output to screen
+        cin >> arr[i]; //input from user
+    }
+}
+void bubblesortArray()
+{
+     //Procedure to sort the arrahy using bubble sort method 
+     int pass = 1; //step 1
+
+     do 
+     {
+        for (int j = 0; j <= n -1 - pass; j++)
+        {
+               //step 2: compare adjacent elements
+               if (arr[j] > arr[j + 1])
+               {
+                 //step 3: swap if out of order
+                 int temp;
+                 temp = arr[j]
+                 arr[j] = arr[j + 1];
+                 arr[j + 1] = temp;
+               }
+        }
+        pass += 1; //step 4: increment pass counter
+        //Display the array after each pass
+        cout << "Pass" << pass - 1 << ": "; //Number of pass
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " "; //Display data for this pass
+        }
+        cout << endl;
+     } while (pass <= n - 1); //step 5: contonute until all passes are done
+    }
+    void display()
+    {
+    cout << endl;
+    cout << "==========================" << endl;
+    cout << "Sorted Array Elements" << endl;
+    cout << "==========================" << endl;
+    cout << endl;
+    }
+     
